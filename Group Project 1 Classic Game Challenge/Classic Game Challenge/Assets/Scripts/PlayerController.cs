38681813b077f41032537f7bb398b12f6bdbe746
NicolasSpohn <149342232+NicolasSpohn@users.noreply.github.com>
2024-01-29 19:12:42 -0500

    First push to main

---
+++ Group Project 1 Classic Game Challenge/Classic Game Challenge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rigi;
+    //public Animator ani;
+    Vector2 playerMovement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player Input
+        playerMovement.x = Input.GetAxisRaw("Horizontal");
+        playerMovement.y = Input.GetAxisRaw("Vertical");
+
+        // Player Animation
+        //ani.SetFloat("Horizontal", playerMovement.x);
+        //ani.SetFloat("Horizontal", playerMovement.y);
+        //ani.SetFloat("Horizontal", playerMovement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        // Player Movement
+        rigi.MovePosition(rigi.position + playerMovement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
