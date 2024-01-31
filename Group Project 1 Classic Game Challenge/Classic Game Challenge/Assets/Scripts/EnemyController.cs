68c4b4ea1c768561a183dad4c80c86fc6cc14bbb
NicolasSpohn <149342232+NicolasSpohn@users.noreply.github.com>
2024-01-30 22:00:51 -0500

    Modified enemy script

---
+++ Group Project 1 Classic Game Challenge/Classic Game Challenge/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    
+    [SerializeField] private float attackDamage = 50f;
+    // Enemy will only be able to take every half second
+    //[SerializeField] private float attackRate = 0.5f;
+    //private float attackAble;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This function will detect if the enemy collides with the "Player" game object. If it does, damage will be dealt to the player and the enemy will destroy on collision. 
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        /*if (other.gameObject.tag == "Player")
+        {
+            if (attackRate <= attackAble)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+                attackAble = 0f; 
+            }
+            else
+            {
+                attackAble += Time.deltaTime;
+            }
+        }*/
+
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+        }
+        Destroy(gameObject);
+    }
+}
