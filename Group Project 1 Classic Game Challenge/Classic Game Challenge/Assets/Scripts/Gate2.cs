81c1335de277df967946d8333df17d8e33f6b6fb
NicolasSpohn <149342232+NicolasSpohn@users.noreply.github.com>
2024-02-05 21:29:35 -0500

    Door working

--- Group Project 1 Classic Game Challenge/Classic Game Challenge/Assets/Scripts/Gate2.cs
+++ Group Project 1 Classic Game Challenge/Classic Game Challenge/Assets/Scripts/Gate2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gate1 : MonoBehaviour
+public class Gate2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,7 @@ public class Gate1 : MonoBehaviour
         PlayerController inventory = other.gameObject.GetComponent<PlayerController>();
         if (other.gameObject.tag == "Player")
         {
-            if(inventory.keyCount == 1)
+            if(inventory.keyCount == 2)
             {
                 Destroy(gameObject);
             }
